f8daa8ee42aea1c56a93b2b86948b603fdd45414
iammahm00d <96152208+iammahm00d@users.noreply.github.com>
2026-06-06 12:59:24 +0300

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  // Relative asset paths make the build work on any GitHub Pages repo URL:
-  // https://username.github.io/any-repo-name/
-  base: './',
-});
